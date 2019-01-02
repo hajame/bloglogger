9d81e7860f9c6f188abbd62607fc2f785ee902c3
Harri Mehtälä <harri.mehtala@helsinki.fi>
2019-01-02 14:44:05 +0200

    teht 4.2 -- controller

--- index.js
+++ index.js
@@ -5,29 +5,31 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 module.exports = Blog
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+// app.get('/api/blogs', (request, response) => {
+//   Blog
+//     .find({})
+//     .then(blogs => {
+//       response.json(blogs)
+//     })
+// })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+// app.post('/api/blogs', (request, response) => {
+//   const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+//   blog
+//     .save()
+//     .then(result => {
+//       response.status(201).json(result)
+//     })
+// })
 
 const PORT = 3003
 app.listen(PORT, () => {
