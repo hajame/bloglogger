928e43b2b3a984e4616aa93450fc2a7aacd57776
Harri Mehtälä <harri.mehtala@helsinki.fi>
2019-01-02 14:44:37 +0200

    teht 4.2 -- controller

--- index.js
+++ index.js
@@ -13,24 +13,6 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use('/api/blogs', blogsRouter)
 
-// app.get('/api/blogs', (request, response) => {
-//   Blog
-//     .find({})
-//     .then(blogs => {
-//       response.json(blogs)
-//     })
-// })
-
-// app.post('/api/blogs', (request, response) => {
-//   const blog = new Blog(request.body)
-
-//   blog
-//     .save()
-//     .then(result => {
-//       response.status(201).json(result)
-//     })
-// })
-
 const PORT = 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
