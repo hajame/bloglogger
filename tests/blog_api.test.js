8ddf3b8193e2c973ed3b4686e7037b0d50062d50
Harri Mehtälä <harri.mehtala@helsinki.fi>
2019-01-08 13:50:20 +0200

    4.12 test refactoring

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -12,82 +12,88 @@ describe('api tests', () => {
     await Promise.all(blogObjects.map(b => b.save()))
   })
 
-  test('blogs are returned as json', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
+  describe('GET-method tests', () => {
+
+    test('blogs are returned as json', async () => {
+      await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    })
+
+    test('all blogs are returned', async () => {
+      const blogsInDatabase = await blogsInDb()
+      const response = await api
+        .get('/api/blogs')
+
+      expect(response.body.length).toBe(blogsInDatabase.length)
+    })
+
+    test('a specific blog is within the returned blogs', async () => {
+      const response = await api
+        .get('/api/blogs')
+
+      const titles = response.body.map(r => r.title)
+
+      expect(titles).toContain('Descrambling split-band voice inversion with deinvert')
+    })
   })
 
-  test('all blogs are returned', async () => {
-    const blogsInDatabase = await blogsInDb()
-    const response = await api
-      .get('/api/blogs')
+  describe('POST-method tests', () => {
+    
+    test('a valid blog can be added', async () => {
+      const blogsAtStart = await blogsInDb()
+      const newBlog = {
+        title: 'This is a valid blog',
+        author: 'Test',
+        url: 'https://www.test.test/',
+        likes: 9
+      }
 
-    expect(response.body.length).toBe(blogsInDatabase.length)
-  })
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-  test('a specific blog is within the returned blogs', async () => {
-    const response = await api
-      .get('/api/blogs')
+      const blogsAfterOperation = await blogsInDb()
 
-    const titles = response.body.map(r => r.title)
+      const titles = blogsAfterOperation.map(r => r.title)
 
-    expect(titles).toContain('Descrambling split-band voice inversion with deinvert')
-  })
+      expect(blogsAfterOperation.length).toBe(blogsAtStart.length + 1)
+      expect(titles).toContain('This is a valid blog')
+    })
 
-  test('a valid blog can be added', async () => {
-    const blogsAtStart = await blogsInDb()
-    const newBlog = {
-      title: 'This is a valid blog',
-      author: 'Test',
-      url: 'https://www.test.test/',
-      likes: 9
-    }
+    test('a blog without likes prop has 0 likes', async () => {
+      const newBlog = {
+        title: 'This is a valid blog',
+        author: 'Test',
+        url: 'https://www.test.test/'
+      }
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+      const blogsAfterOperation = await blogsInDb()
+      const likes = blogsAfterOperation.map(r => r.likes)
+      expect(likes).toContain(0)
+    })
 
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
+    test('a blog without title or url is not added', async () => {
+      const newBlog = {
+        author: 'Test',
+        likes: 55
+      }
+      const blogsAtStart = await blogsInDb()
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+      const blogsAfterOperation = await blogsInDb()
 
-    const blogsAfterOperation = await blogsInDb()
-
-    const titles = blogsAfterOperation.map(r => r.title)
-
-    expect(blogsAfterOperation.length).toBe(blogsAtStart.length + 1)
-    expect(titles).toContain('This is a valid blog')
-  })
-
-  test('a blog without likes prop has 0 likes', async () => {
-    const newBlog = {
-      title: 'This is a valid blog',
-      author: 'Test',
-      url: 'https://www.test.test/'
-    }
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-    const blogsAfterOperation = await blogsInDb()
-    const likes = blogsAfterOperation.map(r => r.likes)
-    expect(likes).toContain(0)
-  })
-
-  test('a blog without title or url is not added', async () => {
-    const newBlog = {
-      author: 'Test',
-      likes: 55
-    }
-    const blogsAtStart = await blogsInDb()
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
-    const blogsAfterOperation = await blogsInDb()
-
-    expect(blogsAfterOperation.length).toBe(blogsAtStart.length)
+      expect(blogsAfterOperation.length).toBe(blogsAtStart.length)
+    })
   })
 
   afterAll(() => {
